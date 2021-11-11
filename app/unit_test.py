281b1a7037918acf5d362b52ca27d86c8edcd5a0
lfullenkamp <73202404+lfullenkamp@users.noreply.github.com>
2021-11-11 12:13:20 -0500

    US1 - warranty system database

--- app/unit_test.py
+++ app/unit_test.py
@@ -1,3 +1,4 @@
+import mysql.connector
 from random import choice
 from string import ascii_letters, digits
 from unittest import TestCase
@@ -7,9 +8,16 @@ random_string = ''.join(choice(ascii_letters) for x in range(10))
 random_numbers = ''.join(choice(digits) for y in range(10))
 random_digit = ''.join(choice(digits) for z in range(1))
 
+# Database connection
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    database="service"
+)
 
-# Prevents inputs that only contain spaces from being entered into the database
-def check_input(test_input):
+
+# Prevents inputs from only contain spaces from being entered into the database
+def check_user_input(test_input):
     if not test_input or len(test_input) == 0 or test_input.isspace():
         return False
     else:
@@ -17,16 +25,15 @@ def check_input(test_input):
 
 
 class IndexTest(TestCase):
-    # Tests the check_input function
+    # Tests the check_user_input function
     def test_check_input(self):
-        # Assert that normal input will be entered into the database
-        self.assertTrue(check_input(random_string))
-        self.assertTrue(check_input(random_digit))
-        self.assertTrue(check_input(random_numbers))
+        # Asserts normal input will be entered into the database
+        self.assertTrue(check_user_input(random_string))
+        self.assertTrue(check_user_input(random_digit))
+        self.assertTrue(check_user_input(random_numbers))
         print('check_input() TRUE test -> PASSED' + '\n')
-        # Assert that null or empty spaces alone will not be entered into the database
-        self.assertFalse(check_input(''))
+        # Asserts null or empty spaces will not be entered into the database
+        self.assertFalse(check_user_input(''))
         print('check_input() Empty Input test -> PASSED' + '\n')
-        self.assertFalse(check_input(' '))
+        self.assertFalse(check_user_input(' '))
         print('check_input() Space Input test -> PASSED' + '\n')
-
--- app/views.py
+++ app/views.py
@@ -38,18 +38,19 @@ def manufacturing():
         h2s = request.form['h2s']
         h2s_warranty = request.form['h2s warranty']
         h2s_date = request.form['h2s date']
+        h2n = request.form['h2n']
+        h2n_warranty = request.form['h2n warranty']
+        h2n_date = request.form['h2n date']
         # Initializes database cursor
         cursor = db.cursor()
         # Inserts every user entry into the database
         cursor.execute('INSERT INTO service.service (`instrument`, `serial number`, `customer name`,`date`, '
                        '`warranty life`, `co`, `co warranty`, `co date`, `o2`,`o2 warranty`, `o2 date`, `h2s`, '
-                       '`h2s warranty`, `h2s date`) '
-                       'VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
-                       (instrument, serial_number, customer_name,
-                        date,
-                        warranty_life, co, co_warranty, co_date,
-                        o2, o2_warranty, o2_date,
-                        h2s, h2s_warranty, h2s_date))
+                       '`h2s warranty`, `h2s date`, `h2s`, `h2s warranty`, `h2s date`) '
+                       'VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
+                       (instrument, serial_number, customer_name, date, warranty_life, co, co_warranty, co_date, o2,
+                        o2_warranty, o2_date, h2s, h2s_warranty, h2s_date, h2n, h2n_warranty, h2n_date))
+        # Commits user input into database
         db.commit()
     return render_template("index.html")
 
